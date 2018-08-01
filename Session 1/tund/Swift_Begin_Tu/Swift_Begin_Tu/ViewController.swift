7668a8a551d55cf316642166e2f1f8beefa40d76
TuNguyen <tunguyen090896@gmail.com>
2018-08-01 14:17:49 +0700

    init project

---
+++ Session 1/tund/Swift_Begin_Tu/Swift_Begin_Tu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift_Begin_Tu
+//
+//  Created by admin on 8/1/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("View did load")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
