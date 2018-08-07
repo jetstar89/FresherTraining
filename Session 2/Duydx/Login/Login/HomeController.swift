ef58d271f1bc25e0244e8015f0024b35c4633549
DuyIoT <duyaries96@gmail.com>
2018-08-07 16:13:38 +0700

    Duydx: Insert MapView

--- Session 2/Duydx/Login/Login/HomeController.swift
+++ Session 2/Duydx/Login/Login/HomeController.swift
@@ -5,10 +5,12 @@
 //  Created by admin on 8/6/18.
 //  Copyright © 2018 admin. All rights reserved.
 //
-
+import MapKit
 import UIKit
 
 class HomeController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,5 +31,10 @@ class HomeController: UIViewController {
         navigationItem.title = "Home"
         navigationController?.navigationBar.barTintColor = UIColor(red: 63/255, green: 95/255, blue: 163/255, alpha: 1.0)
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        let latitude:CLLocationDegrees = 21.016919
+        let longitude:CLLocationDegrees = 105.781865
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let region = MKCoordinateRegionMakeWithDistance(location, 500, 500)
+        mapView.setRegion(region, animated: false)
     }
 }
