79b3882ecdd770b6331cc399a46868dc4f0c395d
Đinh Ngọc Vũ <vudn.it@gmail.com>
2018-08-10 09:54:34 +0700

    Fix commit error

---
+++ Session 2/Duydx/Login/Login/FunctionTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FunctionTableViewCell.swift
+//  Login
+//
+//  Created by admin on 8/9/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class FunctionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var logoImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Session 2/Tund/Session2/Session2/MenuTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MenuTableViewCell.swift
+//  Session2
+//
+//  Created by admin on 8/9/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+    @IBOutlet weak var functionLabel: UILabel!
+
+    @IBOutlet weak var functionImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
