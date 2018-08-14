6f750746c59e51bc871f72e5f0a49f79e989400c
DuyIoT <duyaries96@gmail.com>
2018-08-14 09:22:58 +0700

    Duydx: Fix error show TableView Screen TimeKeepingViewController

--- Session 2/Duydx/Login/Login/TimeKeepingViewController.swift
+++ Session 2/Duydx/Login/Login/TimeKeepingViewController.swift
@@ -53,13 +53,13 @@ class TimeKeepingViewController: UIViewController {
         childHeaderTableView = [
             [DayOfWeek("T2", "10", "08:30 SA", "05:30 CH", "1", "Làm ngoài giờ"),
              DayOfWeek("T3", "11", "08:30 SA", "08:30 SA", "1", "Làm ngoài giờ"),
-             DayOfWeek("T4", "12", "08:30 SA", "Quên chấm công?", "1", "Làm ngoài giờ"),
+             DayOfWeek("T4", "12", "08:30 SA", "Quên chấm công?", "1", "Công ty"),
              DayOfWeek("T5", "13", "08:30 SA", "12:00 SA", "1", "Làm ngoài giờ"),
              DayOfWeek("T6", "14", "-", "-", "1", "Làm ngoài giờ"),
              DayOfWeek("T7", "15", "08:30 SA", "Quên chấm công?", "1", "Làm ngoài giờ"),
             ],
             [DayOfWeek("T2", "10", "08:30 SA", "05:30 CH", "1", "Làm ngoài giờ"),
-             DayOfWeek("T3", "11", "08:30 SA", "05:30 CH", "1", "Làm ngoài giờ"),
+             DayOfWeek("T3", "11", "08:30 SA", "05:30 CH", "1", "Công ty"),
              DayOfWeek("T4", "12", "08:30 SA", "Quên chấm công?", "1", "Làm ngoài giờ"),
              DayOfWeek("T5", "13", "08:30 SA", "05:30 CH", "1", "Làm ngoài giờ"),
              
@@ -70,7 +70,7 @@ class TimeKeepingViewController: UIViewController {
              
              ],
             [DayOfWeek("T2", "10", "-", "-", "1", "Làm ngoài giờ"),
-             DayOfWeek("T3", "11", "08:30 SA", "05:30 CH", "1", "Làm ngoài giờ"),
+             DayOfWeek("T3", "11", "08:30 SA", "05:30 CH", "1", "OT"),
              DayOfWeek("T4", "12", "08:30 SA", "Quên chấm công?", "1", "Làm ngoài giờ"),
              DayOfWeek("T5", "13", "08:30 SA", "05:30 CH", "1", "Làm ngoài giờ"),
              DayOfWeek("T6", "14", "08:30 SA", "12:00 SA", "1", "Làm ngoài giờ"),
@@ -93,20 +93,23 @@ extension TimeKeepingViewController: UITableViewDataSource {
         }
         cell.dayLabel.text = childHeaderTableView[indexPath.section][indexPath.row].day
         cell.dayOfWeekLabel.text = childHeaderTableView[indexPath.section][indexPath.row].weekday
+        cell.timeCheckInLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkIn
         let timeCheckOut = childHeaderTableView[indexPath.section][indexPath.row].checkOut
         if timeCheckOut.isEqual("Quên chấm công?") {
+            cell.statusLabel.isHidden = false
             cell.dayWarningLabel.isHidden = true
             cell.requestButton.isHidden = true
-            cell.timeCheckInLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkIn
             cell.timeCheckOutLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkOut
             cell.timeCheckOutLabel.backgroundColor = UIColor(red: 207/255, green: 2/255, blue: 27/255, alpha: 1)
             cell.statusLabel.text = ""
             cell.timeDayLabel.text = "0"
             cell.warningRequestLabel.isHidden = true
+            cell.timeCheckOutLabel.textColor = UIColor.black
+            
         } else if timeCheckOut.isEqual("08:30 SA"){
+            cell.statusLabel.isHidden = false
             cell.dayWarningLabel.isHidden = true
             cell.requestButton.isHidden = true
-            cell.timeCheckInLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkIn
             cell.timeCheckOutLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkOut
             cell.timeCheckOutLabel.textColor = UIColor(red: 245/255, green: 166/255, blue: 35/255, alpha: 1)
             cell.statusLabel.text = ""
@@ -114,38 +117,39 @@ extension TimeKeepingViewController: UITableViewDataSource {
             cell.warningRequestLabel.isHidden = false
             cell.timeCheckOutLabel.backgroundColor = UIColor.clear
         } else if timeCheckOut.isEqual("12:00 SA"){
-            cell.timeCheckInLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkIn
             cell.timeCheckOutLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkOut
             cell.timeDayLabel.text = "0.5"
             cell.statusLabel.isHidden = true
             cell.dayWarningLabel.isHidden = false
             cell.requestButton.isHidden = false
+            cell.warningRequestLabel.isHidden = true
             cell.dayWarningLabel.text = "0.5"
             cell.requestButton.setTitle("P", for: .normal)
             cell.timeCheckOutLabel.backgroundColor = UIColor.clear
             cell.requestButton.backgroundColor = UIColor(red: 61/255, green: 182/255, blue: 96/255, alpha: 1)
+            cell.timeCheckOutLabel.textColor = UIColor.black
         } else if timeCheckOut.isEqual("-"){
-            cell.timeCheckInLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkIn
             cell.timeCheckOutLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkOut
             cell.timeDayLabel.text = "0"
             cell.statusLabel.isHidden = true
             cell.dayWarningLabel.isHidden = false
             cell.requestButton.isHidden = false
-            cell.dayWarningLabel.text = "0.5"
-            cell.timeCheckOutLabel.backgroundColor = UIColor.clear
+            cell.warningRequestLabel.isHidden = true
+            cell.dayWarningLabel.text = "1"
             cell.requestButton.setTitle("!", for: .normal)
+            cell.timeCheckOutLabel.backgroundColor = UIColor.clear
             cell.requestButton.backgroundColor = UIColor(red: 245/255, green: 166/255, blue: 35/255, alpha: 1)
+            cell.timeCheckOutLabel.textColor = UIColor.black
         } else {
             cell.statusLabel.isHidden = false
             cell.dayWarningLabel.isHidden = true
             cell.requestButton.isHidden = true
-            cell.timeCheckInLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkIn
             cell.timeCheckOutLabel.text = childHeaderTableView[indexPath.section][indexPath.row].checkOut
             cell.statusLabel.text = childHeaderTableView[indexPath.section][indexPath.row].status
             cell.timeDayLabel.text = childHeaderTableView[indexPath.section][indexPath.row].workday
             cell.warningRequestLabel.isHidden = true
             cell.timeCheckOutLabel.backgroundColor = UIColor.clear
-            cell.timeCheckOutLabel.textColor = UIColor.white
+            cell.timeCheckOutLabel.textColor = UIColor.black
         }
         return cell
     }
@@ -176,3 +180,5 @@ extension TimeKeepingViewController: UITableViewDataSource {
 extension TimeKeepingViewController: UITableViewDelegate {
 
 }
+
+
